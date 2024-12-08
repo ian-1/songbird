4753b3c26a220b520e6487a9c47f43e04925448d
Ian Walton <51008396+ian-1@users.noreply.github.com>
2024-12-08 19:43:50 +0000

    Start project

---
+++ src/main/java/com/portmoor/songbird/SongbirdApplication.java
@@ -0,0 +1,13 @@
+package com.portmoor.songbird;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongbirdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongbirdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portmoor/songbird/SongbirdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portmoor.songbird;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongbirdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
